ed2204c931b180eeca9dd155b4aa0b15171abbbe
ervachon <eric.vachon.pro@orange.fr>
2016-01-06 17:58:16 +0100

    ok

--- server.R
+++ server.R
@@ -1,127 +1,129 @@
-
 suppressPackageStartupMessages(library(shiny))
-suppressPackageStartupMessages(library(RCurl))
-suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(caret))
-suppressPackageStartupMessages(library(googleVis))
+suppressPackageStartupMessages(library(rJava))
+suppressPackageStartupMessages(library(NLP))
+suppressPackageStartupMessages(library(RWeka))
+suppressPackageStartupMessages(library(tm))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(wordcloud))
+suppressPackageStartupMessages(library(slam))
 
-theURL <-"https://archive.ics.uci.edu/ml/datasets/Student+Performance"
+loadData <- function(theData){
+  if (theData == "STOP"){
+     theData <- load(url("https://github.com/ervachon/Capston_Project/raw/gh-pages/data.shiny/wFinal_StopWords.RData"))
+  }
+  else{
+    theData <- load(url("https://github.com/ervachon/Capston_Project/raw/gh-pages/data.shiny/wFinal_noStopWords.RData"))    
+  }
+  print("ok")
+  return(wFinal)
+}
+
+returnSentenceNGramMax <- function(laPhrase) {
+  return(returnSentence(laPhrase,nbGram-1))
+}
+
+returnSentence <- function(laPhrase,n) {
+  #clean the sentence and take the n last words  
+  laPhrase <- iconv(laPhrase, "latin1", "ASCII", sub=" ");
+  laPhrase <- gsub("[^[:alpha:][:space:][:punct:]]", "", laPhrase)
   
-theFormula <- function(theSelected) {
-  f <- returnFeaturesSelected(theSelected)
-  return( f )
-}
-
-loadCSV <- function(URL) {
-  csv <- tryCatch( {read.csv(text=getURL(URL,ssl.verifypeer=0L, followlocation=1L),sep=';')},
-                          error=function(cond) {return(read.csv(URL,sep=';'))})    
-  return(csv)
-}
-
-SelectStudentData <- function(theData){
-  if (theData == "Portugues") {
-    studentData     <- loadCSV("https://raw.githubusercontent.com/ervachon/Developing_Data_Products/gh-pages/student/student-por.csv")   
-  } else {
-    studentData     <- loadCSV("https://raw.githubusercontent.com/ervachon/Developing_Data_Products/gh-pages/student/student-mat.csv")   
+  theSentence <- VCorpus(VectorSource(laPhrase))
+  theSentence <- tm_map(theSentence, content_transformer(tolower))
+  theSentence <- tm_map(theSentence, removePunctuation)
+  theSentence <- tm_map(theSentence, removeNumbers)
+  theSentence <- tm_map(theSentence, stripWhitespace)
+  
+  # attention not always
+  if (cleanStopWord==TRUE){
+    theSentence <- tm_map(theSentence, function(x)removeWords(x,stopwords()))
   }
   
-  studentData$ageLevel <- as.factor(studentData$age)
-  return(studentData)
+  theSentence <- tm_map(theSentence, stripWhitespace)
   
+  # here we take only the n last words
+  tmp <- trimws(as.character(theSentence[[1]]))
+  tmp <- strsplit(trimws(tmp), " ")
+  
+  #split avec espace les n derniers
+  tmp <- tail(tmp[[1]],n)
+  tmp <- paste(tmp, collapse = ' ')
+  
+  return(tmp)
 }
 
-theColors <- function(indice){
-  col <- array(data <- c("red", "orange", "blue", "green", "brown", "yellow", "pink"))
-  return(col[as.integer(indice)])
-} 
+returnFirstWords <- function(listWords){
+  strTmp <- strsplit(trimws(listWords), " ")[[1]]
+  return(paste(strTmp[1:(length(strTmp)-1)], collapse = ' '))
+}
 
-myPredict <- function(myData,theSelectedFeatures){
-  set.seed(12345)
-  inTrain           <- createDataPartition(y=myData$G3, p=.8, list = FALSE)
-  ourTrain          <- myData[inTrain,]
-  ourTest           <- myData[-inTrain,]
-  modelFit          <- train(as.formula(theFormula(theSelectedFeatures)), method = "glm",data=ourTrain)
-  testPred          <- predict(modelFit,ourTest)
-  results           <- cbind(ourTest$G3,testPred)
-  colnames(results) <- c("Reference", "Prediction")
+returnLastWord <- function(listWords){
+  strTmp <- strsplit(trimws(listWords), " ")[[1]]
+  return(strTmp[length(strTmp)])
+}
+
+returnNbGram <- function(word){
+  return(length(strsplit(word," ")[[1]]))
+}
+
+returnFrequenceNgram <- function(theNGram){
+  n <- returnNbGram(theNGram)
+  return (wFinal[[n]][[1]]$freq[match(theNGram, wFinal[[n]][[1]]$terms[])])
+}
+
+returnCountNgramCorpus <- function(theNGram){
+  return (wFinal[[n]][[1]]$freq[match(theNGram, wFinal[[n]][[1]]$terms[])])
+}
+
+returnListNGramOfNgramLessOne <- function(theNgram){
+  n <- returnNbGram(theNgram)
+  listTmp <- wFinal[[n+1]][[1]][grep(paste("^",theNgram," ",sep=""),
+                                     wFinal[[n+1]][[1]]$terms), ] 
+  tmp <- c()
+  if (nrow(listTmp)>0){tmp <-listTmp[1:min(nbResAnalyse,nrow(listTmp)),]}
+  return(tmp)
+}
+
+predictMLE <- function(theSentence){
+  res <- c()
+  tmp <- returnListNGramOfNgramLessOne(theSentence)
+  if (is.null(tmp)!=TRUE){
+    for (i in 1:(min(nbRes,nrow(tmp)))){
+      res <- c(res,returnLastWord(tmp[i,]$terms))
+    }
+  }
+  return(res)
+}
+
+predictBackOFF <- function(theSentence){
+  tmp <- strsplit(trimws(theSentence), " ")
+  listSentence <- vector(mode="list", length=nbGram-1)
+  for (i in 1:(nbGram-1)){
+    listSentence[[i]] <- paste(tmp[[1]][i:(nbGram-1)], collapse = ' ')
+  }
+  res <- predictMLE(theSentence[1])
+  i <- 2
+  while(length(res)<=nbRes & i < nbGram ){
+    resTmp <- predictMLE(listSentence[[i]])
+    if (length(resTmp)>0){
+      for (j in 1:length(resTmp)){
+        if ((resTmp[j] %in% res)==FALSE){res <- c(res,resTmp[j])}
+      }
+    }
+    i<-i+1
+  }
+  return(res[1:(min(nbRes,length(res)))])
+}
+
+myPredict <- function(myData,theSentence){
+  result <- "OK"
   return(as.data.frame(results))
-}
-
-returnFeaturesSelected <- function(theSelected) {
-  formula <- ""
-  if (1 %in% theSelected) { formula <- paste(formula," + " ,"school")}
-  if (3 %in% theSelected) { formula <- paste(formula," + " ,"age")}
-  if (4 %in% theSelected) { formula <- paste(formula," + " ,"address")}
-  if (5 %in% theSelected) { formula <- paste(formula," + " ,"famsize")}
-  if (6 %in% theSelected) { formula <- paste(formula," + " ,"Pstatus")}
-  if (7 %in% theSelected) { formula <- paste(formula," + " ,"Medu")}
-  if (8 %in% theSelected) { formula <- paste(formula," + " ,"Fedu")}
-  if (9 %in% theSelected) { formula <- paste(formula," + " ,"Mjob")}
-  if (10 %in% theSelected) { formula <- paste(formula," + " ,"Fjob")}
-  if (11 %in% theSelected) { formula <- paste(formula," + " ,"reason")}
-  if (12 %in% theSelected) { formula <- paste(formula," + " ,"guardian")}
-  if (13 %in% theSelected) { formula <- paste(formula," + " ,"traveltime")}
-  if (14 %in% theSelected) { formula <- paste(formula," + " ,"studytime")}
-  if (15 %in% theSelected) { formula <- paste(formula," + " ,"failures")}
-  if (16 %in% theSelected) { formula <- paste(formula," + " ,"schoolsup")}
-  if (17 %in% theSelected) { formula <- paste(formula," + " ,"famsup")}
-  if (18 %in% theSelected) { formula <- paste(formula," + " ,"paid")}
-  if (19 %in% theSelected) { formula <- paste(formula," + " ,"activities")}
-  if (20 %in% theSelected) { formula <- paste(formula," + " ,"nursery")}
-  if (21 %in% theSelected) { formula <- paste(formula," + " ,"higher")}
-  if (22 %in% theSelected) { formula <- paste(formula," + " ,"internet")}
-  if (23 %in% theSelected) { formula <- paste(formula," + " ,"romantic")}
-  if (24 %in% theSelected) { formula <- paste(formula," + " ,"famrel")}
-  if (25 %in% theSelected) { formula <- paste(formula," + " ,"freetime")}
-  if (26 %in% theSelected) { formula <- paste(formula," + " ,"goout")}
-  if (27 %in% theSelected) { formula <- paste(formula," + " ,"Dalc")}
-  if (28 %in% theSelected) { formula <- paste(formula," + " ,"Walc")}
-  if (29 %in% theSelected) { formula <- paste(formula," + " ,"health")}
-  if (30 %in% theSelected) { formula <- paste(formula," + " ,"absences")}
-  if (31 %in% theSelected) { formula <- paste(formula," + " ,"G1")}
-  if (32 %in% theSelected) { formula <- paste(formula," + " ,"G2")}
-  
-  return(paste("G3 ~ ",substring(formula,4)))
 }
 
 shinyServer(
   function(input, output) {    
-    set.seed(12345)
-        
-    myData <- reactive({SelectStudentData(input$data)})
-    
-    output$selFormula      <- renderText({paste("The formula use in the generalized linear model (method='glm', Caret package) with the data ", input$data," is :")})
-    output$selected        <- renderText({paste("You have selected : ", input$data)})
-    output$showed          <- renderText({paste("Data : ", input$data)})
-    output$theDataSelected <- renderDataTable({data <- myData()})
-    output$ageHist         <- renderPlot(ggplot(myData(), aes(x=ageLevel,fill=sex))
-                                            + geom_bar(binwidth=.5,position="dodge")
-                                            + facet_grid(school ~ . )
-                                            + xlab ("Age of the students")
-                                            + scale_fill_manual(values=c( theColors(input$col1), theColors(input$col2)))
-                                         )
-    
-    myPred      <- reactive({myPredict(myData(),
-                                       if (input$updSelFeat==0)
-                                          {c(1:32)}
-                                       else
-                                          {isolate ({c(input$featuresSel1,input$featuresSel2)})}
-                                      )
-                            })
-    output$pred <- renderPlot( ggplot(myPred(), aes(x = Reference, y = Prediction)) 
-                                   + geom_point(color='blue')
-                                   + geom_abline(intercept=0,slope=1,colour='red') 
-                                   + geom_smooth(color = 'green')
-                             )
-    output$featSel <- renderText({
-      input$updSelFeat
-      isolate({returnFeaturesSelected(c(input$featuresSel1,input$featuresSel2))})
-      })
-    
-    dfUniv      <- reactive({data.frame(longLat=c("41.5608:-8.3968"),Tip=c("University of Minho, Guimarães, Portugal"))})
-    output$univ <- renderGvis({gvisMap(dfUniv(), "longLat" , "Tip", options=list(showTip=TRUE,showLine=TRUE, 
-                                                                           enableScrollWheel=TRUE,
-                                                                           mapType='normal', useMapTypeControl=TRUE,
-                                                                           zoomLevel=15,width=400, height=400))})
+    myData          <- reactive({loadData("STOP")})
+    output$selected <- renderText({paste("You have selected : ", input$data)})
+    myPred          <- reactive({myPredict(myData(),"this is a" )})  #input$featuresSel1)})
+    output$res      <- renderText({paste("the res : ", myData)})
   }
 )
--- ui.R
+++ ui.R
@@ -1,76 +1,18 @@
 # library(shiny)
-# setwd("D:\\_MOOC_\\git\\Developing_Data_Products")
+# setwd("D:\\_GIT_\\Capston_Project")
 # runApp() 
-# shinyapps::deployApp('D:\\_MOOC_\\git\\Developing_Data_Products')
+# shinyapps::deployApp('D:\\_GIT_\\Capston_Project')
 
 library(shiny)
 
 shinyUI(pageWithSidebar(
-   headerPanel(HTML('Developing Data Products Project<h3>Eric VACHON - October 2015')),
+   headerPanel(HTML('Capston Project <h3>Eric VACHON - January 2016')),
    sidebarPanel( #width = 12,
-    tabsetPanel(
       tabPanel("Common",
-      selectInput("data", 
-                label = "Choose the course data",
-                choices = list("Portugues","Math"),
-                selected = "Portugues"
-      ),
-      br(),
-      h4("Select colors of Histogram"),
-        fluidRow(
-          column(4,radioButtons("col1", label = h4("Female"),
-                                 choices = list("red"   = 1, "orange" = 2, "blue"   = 3,
-                                                "green" = 4, "brown"  = 5, "yellow" = 6,
-                                                "pink"  = 7), selected = 1)),
-          column(4,radioButtons("col2", label = h4("Male"),
-                                 choices = list("red"   = 1, "orange" = 2, "blue"   = 3,
-                                                "green" = 4, "brown"  = 5, "yellow" = 6,
-                                                "pink"  = 7), selected = 3))
-      ),
-      
-      textOutput("col1Sel"),
-      textOutput("col2Sel"),
-      br(),
-      
-      p("The data source : "),
-      a("UC Irvine Machine Learning Repository", 
-        href="https://archive.ics.uci.edu/ml/datasets/Student+Performance"),
-      br(),
-      a("(Download source)", 
-         href="https://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip")
-   ),
-   tabPanel("features selection",
-            br(),
-            actionButton("updSelFeat", "Update the selected features"),
-            fluidRow(
-              column(4, checkboxGroupInput("featuresSel1", 
-                               label = h3("Select"), 
-                               choices = list("school" = 1,"sex" = 2,"age" = 3,"address" = 4,"famsize" = 5,
-                                              "Pstatus" = 6,"Medu" = 7,"Fedu" = 8,"Mjob" = 9,"Fjob" = 10,
-                                              "reason" = 11,"guardian" = 12,"traveltime" = 13,"studytime" = 14,
-                                              "failures" = 15,"schoolsup" = 16), selected = c(1:16)
-                               )
-              ),
-              column(4, checkboxGroupInput("featuresSel2", 
-                                            label = h3("features"), 
-                                            choices = list("famsup" = 17,"paid" = 18,
-                                                           "activities" = 19,"nursery" = 20,"higher" = 21,"internet" = 22,
-                                                           "romantic" = 23,"famrel" = 24,"freetime" = 25,"goout" = 26,
-                                                           "Dalc" = 27,"Walc" = 28,"health" = 29,"absences" = 30,"G1" = 31,
-                                                           "G2" = 32), selected = c(17:32)
-              )
-             )
-            )
-    ))),
+      h4("hello"))),
    mainPanel(#width = 12,
       tabsetPanel(
-        tabPanel("Documentation"              , includeHTML("./www/documentation.html")),
-        tabPanel("Histograms"                 , textOutput("selected"), plotOutput("ageHist")),
-        tabPanel("Prediction"                 , textOutput("selFormula"), textOutput("featSel"), plotOutput("pred")),
-        tabPanel("Browse data"                , textOutput("showed"), dataTableOutput(outputId="theDataSelected")),
-        tabPanel("Source and data information", includeHTML("./www/source.html")),
-        tabPanel("Map of the University"      , h4("University of Minho, Guimarães, Portugal, (41.5608°N 8.3968°W)"), htmlOutput("univ"))
-        )
-      )
-   )
-)
+        tabPanel("PredictOWords"  , textOutput("selected"), plotOutput("ageHist"))
+        ,tabPanel("Documentation"  , includeHTML("./www/documentation.html"))
+        ))
+))
