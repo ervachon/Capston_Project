0f083eede703a3206735aa1a785b15820a7d7b73
ervachon <eric.vachon.pro@orange.fr>
2016-01-17 17:55:07 +0100

    last commit !

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -1,10 +1,6 @@
-#load(url("https://github.com//ervachon//Capston_Project//raw//gh-pages//data.shiny//wFinal_StopWords.RData"))
-#load(url("https://github.com//ervachon//Capston_Project//raw//gh-pages//data.shiny//wFinal_noStopWords.RData"))
-
 #can i know just
 #avec : i know just   : how/the/what/in/a/to/like
 #sans : can know just : case/dont/like/much/want/one/thought
-
 
 nbGram <- 4
 nbRes <- 8
@@ -12,46 +8,19 @@ nbResAnalyse <- nbRes
 
 SelectData.new <- function(noClean){
   show("myText")
-  if (noClean == FALSE) {
-        data <- readRDS('./www/wFinal_noStopWords.RDs')
+  if (noClean == TRUE) {
+    #download.file("https://github.com//ervachon//Capston_Project//raw//gh-pages//data.shiny//wFinal_noStopWords.RDs", destfile = "./www/wFinal_noStopWords.RDs", mode="wb")
+    data <- readRDS('./www/wFinal_noStopWords.RDs')    
   } else {
-        data <- readRDS('./www/wFinal_StopWords.RDs')
+    #download.file("https://github.com//ervachon//Capston_Project//raw//gh-pages//data.shiny//wFinal_StopWords.RDs", destfile = "./www/wFinal_StopWords.RDs", mode="wb")
+    data <- readRDS('./www/wFinal_StopWords.RDs')    
   }
   hide("myText")
   return(data)
 }
 
-SelectData.old <- function(noClean){
-  if (noClean == FALSE) {
-    withProgress(message = paste('##################################',
-                                 'LOAD NO STOPWORD CORPUS',
-                                 '##################################'), 
-                 value = 0,
-               { data <- readRDS('./www/wFinal_noStopWords.RDs') 
-                 #incProgress(1,detail = "########## OK ##########")
-                })
-  } else {
-    withProgress(message = paste('################################## ',
-                                 'LOAD STOPWORD CORPUS',
-                                 '################################## '), 
-                 value = 0,
-               { data <- readRDS('./www/wFinal_StopWords.RDs') 
-                 #incProgress(1,detail = "########## OK ##########")
-               })
-  }
-  return(data)
-}
-
-myLoad <- function(URL) {
-  rds <- tryCatch( {read.csv(text=getURL(URL,ssl.verifypeer=0L, followlocation=1L),sep=';')},
-                   error=function(cond) {return(read.csv(URL,sep=';'))})    
-  return(rds)
-}
-
-
 shinyServer(
   function(input, output,session) { 
-    
     returnSentenceNGramMax <- function(laPhrase,clean) {
       return(returnSentence(laPhrase,nbGram-1,clean))
     }
@@ -126,18 +95,16 @@ shinyServer(
         }
         i<-i+1
       }
-      
       i<-1
       while (length(res) < nbRes){
         if ((wFinal()[[1]][[1]]$terms[i] %in% res)== FALSE){
           res <- c(res,wFinal()[[1]][[1]]$terms[i])}
         i <- i + 1
       }
-      
-      if (theSentence==""){res <- c('-','-' ,'-', '-','-','-','-','-')}
-      
+      if (theSentence==""){res <- c('-','-','-','-','-','-','-','-')}
       return(res[1:(min(nbRes,length(res)))])
     }
+    
     returnFrequenceNgram <- function(theNGram){
       n <- returnNbGram(theNGram)
       return (wFinal()[[n]][[1]]$freq[match(theNGram, wFinal()[[n]][[1]]$terms[])])
@@ -149,14 +116,15 @@ shinyServer(
     
     returnListNGramOfNgramLessOne <- function(theNgram){
       n <- returnNbGram(theNgram)
-      listTmp <- wFinal()[[n+1]][[1]][grep(paste("^",theNgram," ",sep=""),
-                                         wFinal()[[n+1]][[1]]$terms), ] 
+     listTmp <- wFinal()[[n+1]][[1]][grep(paste("^",theNgram," ",sep=""),wFinal()[[n+1]][[1]]$terms), ] 
       tmp <- c()
       if (nrow(listTmp)>0){tmp <-listTmp[1:min(nbResAnalyse,nrow(listTmp)),]}
       return(tmp)
     }
     
+    #---------------------------------------------------------------------------------------------
     wFinal <- reactive({SelectData.new(input$corpus == "2")})
+    res <- reactiveValues(list=c('','' ,'', '','','','',''))
     
     output$predict <- renderText({
           a <- predictBackOFF(returnSentenceNGramMax(input$sentence, input$corpus == "2"))
@@ -166,36 +134,27 @@ shinyServer(
     
     output$triGram <- renderText({returnSentenceNGramMax(input$sentence, input$corpus == "2")})
 
-    output$button_1_4 <- renderUI({
-      lapply(1:4, function(i) {
-        actionButton(inputId = paste0("b_", i), 
-                     label   = res$list[i],
-                     width = '24%')
-      })
-    })
+    #output$button_1_4 <- renderUI({lapply(1:4, function(i) {actionButton(inputId=paste0("b_", i),label=res$list[i],width='24%')})})
+    #output$button_5_8 <- renderUI({lapply(5:8, function(i) {actionButton(inputId=paste0("b_", i),label=res$list[i],width='24%')})})
+    output$button_1 <- renderUI({actionButton(inputId="b_1",label=res$list[1],width='100%')})
+    output$button_2 <- renderUI({actionButton(inputId="b_2",label=res$list[2],width='100%')})
+    output$button_3 <- renderUI({actionButton(inputId="b_3",label=res$list[3],width='100%')})
+    output$button_4 <- renderUI({actionButton(inputId="b_4",label=res$list[4],width='100%')})
+    output$button_5 <- renderUI({actionButton(inputId="b_5",label=res$list[5],width='100%')})
+    output$button_6 <- renderUI({actionButton(inputId="b_6",label=res$list[6],width='100%')})
+    output$button_7 <- renderUI({actionButton(inputId="b_7",label=res$list[7],width='100%')})
+    output$button_8 <- renderUI({actionButton(inputId="b_8",label=res$list[8],width='100%')})
 
-    output$button_5_8 <- renderUI({
-      lapply(5:8, function(i) {
-        actionButton(inputId = paste0("b_", i), 
-                     label   = res$list[i],
-                     width = '24%')
-      })
-    })
-    hide("myText")
-    
-    observeEvent(input[[paste("b_",1,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[1]))})
-    observeEvent(input[[paste("b_",2,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[2]))})
-    observeEvent(input[[paste("b_",3,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[3]))})
-    observeEvent(input[[paste("b_",4,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[4]))})
-    observeEvent(input[[paste("b_",5,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[5]))})
-    observeEvent(input[[paste("b_",6,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[6]))})
-    observeEvent(input[[paste("b_",7,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[7]))})
-    observeEvent(input[[paste("b_",8,sep="")]], {updateTextInput(session, "sentence", value = paste(input[["sentence"]],res$list[8]))})
+    observeEvent(input[["b_1"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[1]))})
+    observeEvent(input[["b_2"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[2]))})
+    observeEvent(input[["b_3"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[3]))})
+    observeEvent(input[["b_4"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[4]))})
+    observeEvent(input[["b_5"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[5]))})
+    observeEvent(input[["b_6"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[6]))})
+    observeEvent(input[["b_7"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[7]))})
+    observeEvent(input[["b_8"]],{updateTextInput(session,"sentence",value=paste(input[["sentence"]],res$list[8]))})
   
-    observe({res$list<-predictBackOFF(returnSentenceNGramMax(input$sentence,
-                                                             input$corpus == "2")) })
-    
-    res <- reactiveValues(list=c('-','-' ,'-', '-','-','-','-','-'))
+    observe({res$list<-predictBackOFF(returnSentenceNGramMax(input$sentence,input$corpus=="2"))})
 
 })
 
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -4,10 +4,10 @@
 # library(shiny)
 # library(shinyapps)
 
-#setwd("D:\\_GIT_\\Capston_Project\\shinyApp")
-#runApp() 
-#Sys.setlocale("LC_ALL","English")
-#shinyapps::deployApp('D:\\_GIT_\\Capston_Project\\shinyApp',appName = "Capston_Project")
+# setwd("D:\\_GIT_\\Capston_Project\\shinyApp")
+# runApp() 
+# Sys.setlocale("LC_ALL","English")
+# shinyapps::deployApp('D:\\_GIT_\\Capston_Project\\shinyApp',appName = "Capston_Project")
 
 #suppressPackageStartupMessages(library(rJava))
 #suppressPackageStartupMessages(library(NLP))
@@ -22,20 +22,21 @@ suppressPackageStartupMessages(library(RCurl))
 
 shinyUI(pageWithSidebar(
    headerPanel(HTML('Capston Project : PredictORama <h3>Eric VACHON - January 2016')),
-   sidebarPanel( #width = 12,
-      tabPanel("Common",
+   sidebarPanel( width = 4,
       radioButtons("corpus",
                    label = h4("Choice your corpus"),
                    choices = list("With Stop Words"= 1,
                                   "Without Stop Words" = 2),
                    selected = 1
                    ),
-      img(src="logo.jpg", width = 253, height = 117),
-      br()
-      )),
+      a(href="https://class.coursera.org/dsscapstone-006",img(src="logo.jpg", width = 253, height = 117)),
+      br(),
+      a(href="https://github.com/ervachon/Capston_Project",img(src="github.png", width = 253, height = 104))
+      #(href="mailto:eric.vachon.pro@orange.fr?&subject=Capton Project Contact",img(src="mail.jpg"))
+   ),
    mainPanel(#width = 12,
       tabsetPanel(
-        tabPanel("PredictORama", 
+        tabPanel(strong("PredictORama"), 
                  
                  useShinyjs(),
                  tags$head(tags$style("#myText{color: red}")),
@@ -44,16 +45,23 @@ shinyUI(pageWithSidebar(
         
                  textInput("sentence", label = h4("Enter your sentence bellow:"), 
                            value='',width = '100%'), 
-#                 verbatimTextOutput("triGram"),
-#                 verbatimTextOutput("predict"),
-                 fluidRow(#column(width=3,actionButton("res1", label="Top 1",width = '100%')),
-                          uiOutput("button_1_4")
-                          ),br(),
-                 fluidRow(#column(width=3,actionButton("res5", label="Top 5",width = '100%')),
-                          uiOutput("button_5_8")
-                         )
-                 )
+                 #verbatimTextOutput("triGram"),
+                 #verbatimTextOutput("predict"),
+                 #fluidRow(uiOutput("button_1_4")),
+                 fluidRow(column(3,uiOutput("button_1")),
+                          column(3,uiOutput("button_2")),
+                          column(3,uiOutput("button_3")),
+                          column(3,uiOutput("button_4"))),
+                 br(),
+                 #fluidRow(uiOutput("button_5_8"))
+                 fluidRow(column(3,uiOutput("button_5")),
+                          column(3,uiOutput("button_6")),
+                          column(3,uiOutput("button_7")),
+                          column(3,uiOutput("button_8")))
+               )
         ,tabPanel("Documentation"  , includeHTML("./www/documentation.html"))
-        ,tabPanel("Milestone Report"  , includeHTML("./www/index.html"))
+        #,tabPanel("Milestone Report"  , includeHTML("http://rpubs.com/ervachon/139299"))
         ))
-))
+    )
+)
+
